a79b681a62bf40f86b0e2409bc416e896612b85d
ragan8 <ragan.gidde@gmail.com>
2020-02-03 23:08:40 +0530

    adding new file

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Monday 03 February 2020 11:08:01  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  NAGARAJ GIDDE (), ragan.gidde@gmail.com
+ *   Organization:  indicidual
+ *
+ * =====================================================================================
+ */
+
+/*
+ * understaning git here
+ */
